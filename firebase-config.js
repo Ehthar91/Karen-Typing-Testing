df77bc0fbcf257f27735f09b7e6508e764bf87f5
GLN <asaw1991@gmail.com>
2026-09-11 20:37:08 -0500

    commit

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD66_ZIACj3eGHpn-HGYfrJHxewthfDoxU",
-  authDomain: "gln-karen-typing-center.firebaseapp.com",
-  databaseURL: "https://gln-karen-typing-center-default-rtdb.firebaseio.com",
-  projectId: "gln-karen-typing-center",
-  storageBucket: "gln-karen-typing-center.firebasestorage.app",
-  messagingSenderId: "250059382069",
-  appId: "1:250059382069:web:f9640cd8f07f1e50e7c07c"
-};
